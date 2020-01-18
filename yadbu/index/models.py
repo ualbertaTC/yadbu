b53116da788ae80b843b64709ff76f9e9557fc0f
Teagan <chouinar@ualberta.ca>
2020-01-18 15:37:24 -0700

    Add database models and database migrations.

--- yadbu/index/models.py
+++ yadbu/index/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Identity(models.Model):
+    patient_number = models.IntegerField()
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+
+class Attributes(models.Model):
+    patient_identity = models.ForeignKey(Identity, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    weight = models.IntegerField()
+
